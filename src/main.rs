5d30171cf05c0365aa3797a0f50ddfc46b790ccb
Michael Telahun <michael.telahun@dindin.et>
2024-03-16 13:31:27 +0300

    refactor: main.rs

--- src/main.rs
+++ src/main.rs
@@ -11,56 +11,52 @@ fn main() {
     println!("Bank Account created.");
 
     loop {
-        write!(
-            io::stdout(),
-            "Please enter transaction type(1: Deposit, 2: Withdrawal, 3: Print Statement): "
-        )
-        .expect("failed to write prompt");
-        io::stdout().flush().expect("failed to flush output buffer");
-
-        let mut input = String::new();
-        io::stdin()
-            .lock()
-            .read_line(&mut input)
-            .expect("failed to read input");
+        let input = solicit_input(
+            "Please enter transaction type(1: Deposit, 2: Withdrawal, 3: Print Statement): ",
+        );
         let input = input.trim();
 
         if input == "1" {
-            write!(io::stdout(), "Enter amount: ").expect("failed to write amount prompt");
-            io::stdout().flush().expect("failed to flush amount prompt");
-            let mut amount = String::new();
-            io::stdin()
-                .lock()
-                .read_line(&mut amount)
-                .expect("failed to read amount");
+            let amount = solicit_input("Enter amount: ");
             let amount = Decimal::from_str(&amount.trim()).expect("unable to parse the amount");
             let _ = bank_account.deposit(amount);
         } else if input == "2" {
-            write!(io::stdout(), "Enter amount: ").expect("failed to write amount prompt");
-            io::stdout().flush().expect("failed to flush amount prompt");
-            let mut amount = String::new();
-            io::stdin()
-                .lock()
-                .read_line(&mut amount)
-                .expect("failed to read amount");
+            let amount = solicit_input("Enter amount: ");
             let amount = Decimal::from_str(&amount.trim()).expect("unable to parse the amount");
             match bank_account.withdraw(amount) {
                 Ok(_) => (),
                 Err(e) => {
                     eprintln!("{e}");
-                    return ()
-                },
+                    return ();
+                }
             }
         } else if input == "3" {
             let statement = bank_account.get_statement();
             println!("|DATE                 | AMOUNT  | BALANCE|");
             println!("|---------------------|---------|--------|");
             for line in statement.lines.iter() {
-                println!(" {}  |   {}   | {} ", line.timestamp.format("%Y-%m-%d %H:%M:%S"), line.amount, line.balance);
+                println!(
+                    " {}  |   {}   | {} ",
+                    line.timestamp.format("%Y-%m-%d %H:%M:%S"),
+                    line.amount,
+                    line.balance
+                );
             }
         } else {
             eprintln!("unable to understand input");
             return ();
         }
     }
+}
+
+fn solicit_input(prompt: &str) -> String {
+    write!(io::stdout(), "{prompt}").expect("failed to write prompt");
+    io::stdout().flush().expect("failed to flush prompt");
+    let mut input = String::new();
+    io::stdin()
+        .lock()
+        .read_line(&mut input)
+        .expect("failed to read amount");
+
+    input.trim().to_owned()
 }
